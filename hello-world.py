279ae7390ab46a4926c8a62a925900cedcfaf5ea
Snehashishdas97 <snehashish.das97@gmail.com>
2023-09-05 21:03:59 -0400

    github

--- hello-world.py
+++ hello-world.py
@@ -1,2 +1,10 @@
-# python program to print "Hello World"
-print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
