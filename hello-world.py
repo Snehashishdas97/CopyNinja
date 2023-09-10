9efd00c1ed9bcfbfb453391a598701c1fb70b433
Snehashishdas97 <snehashish.das97@gmail.com>
2023-09-10 17:11:04 -0400

    new commit

--- hello-world.py
+++ hello-world.py
@@ -2,7 +2,7 @@ from flask import Flask
 import datadog
 
 app = Flask(__name__)
-datadog.initialize(api_key=d4111343-89c5-440a-baa0-a054430b909a)
+#datadog.initialize(api_key=d4111343-89c5-440a-baa0-a054430b909a)
 @app.route('/')
 def hello():
     return "Hello, World!"
